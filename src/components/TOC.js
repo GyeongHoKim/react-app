19e777ae00931ee2f1b41cfead9aa529e61c893e
김경호 <rlarudgh2017@gmail.com>
2021-03-04 03:39:18 +0000

    UPDATE FILES
    for create ReadContent and CreateContent

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
 	render() {
+		var lists = [];
+		var data = this.props.data;
+		for (let i = 0; i < data.length; ++i) {
+			lists.push(
+				<li key={data[i].id}>
+					<a
+						data-id={data[i].id}
+						onClick={(e) => {
+							e.preventDefault();
+							this.props.onChangePage(e.target.dataset.id);
+						}}
+						href={"/content/"+data[i].id}>{data[i].title}
+					</a>
+				</li>);
+		}
 		return (
 			<nav>
 				<ul>
-					<li><a href="1.html">HTML</a></li>
-					<li><a href="2.html">CSS</a></li>
-					<li><a href="3.html">JavaScript</a></li>
+					{lists}
 				</ul>
 			</nav>
 		);
